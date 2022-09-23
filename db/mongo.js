4d7ce6d24f5a3782f141758a71ad7492068f10d7
Gonzalo <garciacalvog@hotmail.com>
2022-09-23 00:14:40 -0300

    prueba

--- db/mongo.js
+++ db/mongo.js
@@ -3,7 +3,7 @@ const moongose = require ('mongoose');
 
 async function connection() {
 
-    const URLstring = `mongodb+srv://lopeza93:River2022@firstcloster.cxeka9h.mongodb.net/entrega10?retryWrites=true&w=majority`
+    const URLstring = `mongodb+srv://garciacalvog:yJrrTE4mcwui4Ed@cluster0.qju9tzm.mongodb.net/?retryWrites=true&w=majority`
 
     const ruta = await moongose.connect(URLstring)
     return ruta
