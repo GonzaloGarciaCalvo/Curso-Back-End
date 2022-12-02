db566ba1213aa166f6595ca40d1affd9f1437ccc
Gonzalo <garciacalvog@hotmail.com>
2022-12-01 22:22:09 -0300

    base desafio clase 42

--- copia/daos/productos/ProductosDaoArchivo.js
+++
@@ -1,9 +0,0 @@
-const ContenedorFirebase = require('../../contenedores/claseContenedor')
-
-
-class ProductosFirebase extends ContenedorFirebase {
-    constructor() {
-        super('productos.txt')
-    }  
-}
-module.exports = ProductosFirebase
